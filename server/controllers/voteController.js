bff7ea226d264917b0f112657b7b4fde771a6110
Luis Carlin <luiscarlin@gmail.com>
2017-08-19 16:34:47 -0400

    Add code to only allow one vote per IP per poll

--- server/controllers/voteController.js
+++ server/controllers/voteController.js
@@ -4,10 +4,33 @@ const voteController = { }
 
 voteController.handlePost = async function (req, res, next) {
   try {
-    await db.models.vote.create({
-      pollOptionId: req.body.pollOptionId
+    const foundVote = await db.models.vote.findOne({
+      where: {
+        voterIp: req.ip,
+        pollOPtionId: {
+          $in: db.sequelize.literal(`
+          (SELECT pollOptions.id
+          FROM pollOptions
+          WHERE pollOptions.pollId = (
+            SELECT pollOptions.pollId
+            FROM pollOptions
+            WHERE pollOptions.id = ${db.sequelize.escape(req.body.pollOptionId)}
+          ))
+        `)
+        }
+      }
     })
-    res.sendStatus(201)
+    if (foundVote === null) {
+      await db.models.vote.create({
+        voterIp: req.ip,
+        pollOptionId: req.body.pollOptionId
+      })
+      res.sendStatus(201)
+    } else {
+      res.status(400).json({
+        message: 'You  have already voted on this poll'
+      })
+    }
   } catch (err) {
     next(err)
   }
--- server/models/vote.js
+++ server/models/vote.js
@@ -1,5 +1,9 @@
 export default function createVoteModel (sequelize, DataTypes) {
   const vote = sequelize.define('vote', {
+    voterIp: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {
     timestamps: false,
     classMethods: {
