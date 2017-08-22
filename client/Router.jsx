0710a291fa15fdfa155586b982e6efe02764857c
Luis Carlin <luiscarlin@gmail.com>
2017-08-22 07:07:23 -0400

    Add vote and result initial pages and routes

--- client/Router.jsx
+++ client/Router.jsx
@@ -4,12 +4,15 @@ import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
 import Layout from './modules/shared/Layout.jsx'
 import CreatePoll from './modules/poll/CreatePoll.jsx'
+import VotePoll from './modules/poll/VotePoll.jsx'
+import ResultPoll from './modules/poll/ResultPoll.jsx'
 
-// issue is here ...
 const rootRoute = (
   <Router history={browserHistory}>
     <Route path='/' component={Layout}>
       <IndexRoute component={CreatePoll} />
+      <Route path='v' component={VotePoll} />
+      <Route path='r' component={ResultPoll} />
     </Route>
   </Router>
 )
---
+++ client/modules/poll/VotePoll.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class VotePoll extends React.Component {
+  render () {
+    return (
+      <div>
+        <h2>VotePoll</h2>
+      </div>
+    )
+  }
+}
+
+module.exports = VotePoll
